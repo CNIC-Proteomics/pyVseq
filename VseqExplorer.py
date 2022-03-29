bd27a5caac18fc82e462c715c8ff87706cbfdcb4
Andrea <andrea.laguillo@externo.cnic.es>
2022-03-29 14:43:08 +0200

    match query to mgf

--- VseqExplorer.py
+++ VseqExplorer.py
@@ -25,10 +25,170 @@ import statistics
 import numpy as np
 pd.options.mode.chained_assignment = None  # default='warn'
 
+# table = 'C:\\Users\\alaguillog\\GitHub\\pyVseq\\vseqexplorer_input_data.csv'
+# config = 'C:\\Users\\alaguillog\\GitHub\\pyVseq\\Vseq.ini'
+
+def getTquery(fr_ns):
+    squery = fr_ns.loc[fr_ns[0].str.contains("SCANS=")]
+    squery = squery[0].str.replace("SCANS=","")
+    squery.reset_index(inplace=True, drop=True)
+    mquery = fr_ns.loc[fr_ns[0].str.contains("PEPMASS=")]
+    mquery = mquery[0].str.replace("PEPMASS=","")
+    mquery.reset_index(inplace=True, drop=True)
+    cquery = fr_ns.loc[fr_ns[0].str.contains("CHARGE=")]
+    cquery = cquery[0].str.replace("CHARGE=","")
+    cquery.reset_index(inplace=True, drop=True)
+    tquery = pd.concat([squery.rename('SCANS'),
+                        mquery.rename('PEPMASS'),
+                        cquery.rename('CHARGE')],
+                       axis=1)
+    tquery[['MZ','INT']] = tquery.PEPMASS.str.split(" ",expand=True,)
+    tquery['CHARGE'] = tquery.CHARGE.str[:-1]
+    tquery = tquery.drop("PEPMASS", axis=1)
+    tquery = tquery.apply(pd.to_numeric)
+    return tquery
+
+def getTheoMZH(sequence, charge, nt, ct):
+    '''    
+    Calculate theoretical MZ using the PSM sequence.
+    '''
+    AAs = dict(mass._sections['Aminoacids'])
+    MODs = dict(mass._sections['Fixed Modifications'])
+    m_proton = mass.getfloat('Masses', 'm_proton')
+    m_hydrogen = mass.getfloat('Masses', 'm_hydrogen')
+    m_oxygen = mass.getfloat('Masses', 'm_oxygen')
+    total_aas = 2*m_hydrogen + m_oxygen
+    total_aas += charge*m_proton
+    if nt:
+        total_aas += float(MODs['nt'])
+    if ct:
+        total_aas += float(MODs['ct'])
+    for aa in sequence:
+        if aa.lower() in AAs:
+            total_aas += float(AAs[aa.lower()])
+        if aa.lower() in MODs:
+            total_aas += float(MODs[aa.lower()])
+        if aa.islower():
+            total_aas += float(MODs['isolab'])
+    MH = total_aas - (charge-1)*m_proton
+    #MZ = (total_aas + int(charge)*m_proton) / int(charge)
+    MZ = total_aas / int(charge)
+    return MZ, MH
+
+def expSpectrum(fr_ns, scan):
+    '''
+    Prepare experimental spectrum.
+    '''
+    index1 = fr_ns.loc[fr_ns[0]=='SCANS='+str(scan)].index[0] + 1
+    index2 = fr_ns.drop(index=fr_ns.index[:index1], axis=0).loc[fr_ns[0]=='END IONS'].index[0]
+    
+    ions = fr_ns.iloc[index1:index2,:]
+    ions[['MZ','INT']] = ions[0].str.split(" ",expand=True,)
+    ions = ions.drop(ions.columns[0], axis=1)
+    ions = ions.apply(pd.to_numeric)
+    ions["ZERO"] = 0
+    #ions["CCU"] = 0.01
+    ions["CCU"] = ions.MZ - 0.01
+    ions.reset_index(drop=True)
+    
+    #bind = pd.DataFrame(list(itertools.chain(*set(zip(list(ions['CCU']),list(ions['MZ']))))), columns=["MZ"])
+    #bind["REL_INT"] = list(itertools.chain(*set(zip(list(ions['ZERO']),list(ions['INT'])))))
+    bind = pd.DataFrame(list(itertools.chain.from_iterable(zip(list(ions['CCU']),list(ions['MZ'])))), columns=["MZ"])
+    bind["REL_INT"] = list(itertools.chain.from_iterable(zip(list(ions['ZERO']),list(ions['INT']))))
+    bind["ZERO"] = 0
+    bind["CCU"] = bind.MZ + 0.01
+    
+    spec = pd.DataFrame(list(itertools.chain.from_iterable(zip(list(bind['MZ']),list(bind['CCU'])))), columns=["MZ"])
+    spec["REL_INT"] = list(itertools.chain.from_iterable(zip(list(bind['REL_INT']),list(bind['ZERO']))))
+    
+    median_rel_int = statistics.median(ions.INT)
+    std_rel_int = np.std(ions.INT, ddof = 1)
+    ions["NORM_REL_INT"] = (ions.INT - median_rel_int) / std_rel_int
+    ions["P_REL_INT"] = scipy.stats.norm.cdf(ions.NORM_REL_INT) #, 0, 1)
+    normspec = ions.loc[ions.P_REL_INT>0.81]
+    spec_correction = max(ions.INT)/statistics.mean(normspec.INT)
+    spec["CORR_INT"] = spec.REL_INT*spec_correction
+    spec.loc[spec['CORR_INT'].idxmax()]['CORR_INT'] = max(spec.REL_INT)
+    spec["CORR_INT"] = spec.apply(lambda x: max(ions.INT)-13 if x["CORR_INT"]>max(ions.INT) else x["CORR_INT"], axis=1)
+    return(spec, ions, spec_correction)
+
+def theoSpectrum(seq, len_ions, dm):
+    '''
+    Prepare theoretical fragment matrix.
+
+    '''
+    m_hydrogen = mass.getfloat('Masses', 'm_hydrogen')
+    m_oxygen = mass.getfloat('Masses', 'm_oxygen')
+    ## Y SERIES ##
+    #ipar = list(range(1,len(seq)))
+    outy = pd.DataFrame(np.nan, index=list(range(1,len(seq)+1)), columns=list(range(1,len_ions+1)))
+    for i in range(0,len(seq)):
+        yn = list(seq[i:])
+        if i > 0: nt = False
+        else: nt = True
+        fragy = getTheoMZH(0,yn,nt,True)[1] + dm
+        outy[i:] = fragy
+        
+    ## B SERIES ##
+    outb = pd.DataFrame(np.nan, index=list(range(1,len(seq)+1)), columns=list(range(1,len_ions+1)))
+    for i in range(0,len(seq)):
+        bn = list(seq[::-1][i:])
+        if i > 0: ct = False
+        else: ct = True
+        fragb = getTheoMZH(0,bn,True,ct)[1] - 2*m_hydrogen - m_oxygen + dm
+        outb[i:] = fragb
+    
+    ## FRAGMENT MATRIX ##
+    yions = outy.T
+    bions = outb.iloc[::-1].T
+    spec = pd.concat([bions, yions], axis=1)
+    spec.columns = range(spec.columns.size)
+    spec.reset_index(inplace=True, drop=True)
+    return(spec)
+
 def main(args):
     '''
     Main function
     '''
+    tol = float(mass._sections['Explorer']['tolerance'])
+    
+    logging.info("Reading input table")
+    seqtable = pd.read_csv(args.table, sep=",", float_precision='high', low_memory=False)
+    logging.info("Reading input file")
+    mgf = pd.read_csv(args.infile, header=None)
+    tquery = getTquery(mgf)
+    
+    for index, query in seqtable.iterrows():
+        logging.info("\tExploring sequence " + str(query.Sequence) + ", "
+                     + str(query.ExpNeutralMass) + " Th, Charge "
+                     + str(query.Charge))
+        ## MZ and MH ##
+        query['MZ'] = getTheoMZH(query.Sequence, query.Charge, True, True)[0]
+        query['MH'] = getTheoMZH(query.Sequence, query.Charge, True, True)[1]
+        ## DM ##
+        mim = query.ExpNeutralMass + mass.getfloat('Masses', 'm_proton')
+        dm = mim - query.MH
+        ## TOLERANCE ##
+        upper = query.MZ + tol
+        lower = query.MZ - tol
+        ## OPERATIONS ##
+        subtquery = tquery[(tquery.CHARGE==query.Charge) & (tquery.MZ>=lower) & (tquery.MZ<=upper)]
+        logging.info("\t" + str(subtquery.shape[0]) + " scans found within ±"
+                     + str(tol) + " Th")
+        logging.info("Comparing...")
+        subtquery['Sequence'] = query.Sequence
+        subtquery['ExpNeutralMass'] = query.ExpNeutralMass
+        subtquery['scan']
+        subtquery['ions_matched']
+        subtquery['ions_total']
+        subtquery['e_score']
+        subtquery['retention_time']
+        subtquery['description']
+    
+    logging.info("Writing output table")
+    outfile = os.path.join(os.path.split(Path(args.table))[0],
+                           os.path.split(Path(args.table))[1][:-4] + "_EXPLORER.csv")
+    
 
 if __name__ == '__main__':
 
@@ -45,7 +205,8 @@ if __name__ == '__main__':
         
     defaultconfig = os.path.join(os.path.dirname(__file__), "config/VseqExplorer.ini")
     
-    parser.add_argument('-i',  '--infile', required=True, help='Input file')
+    parser.add_argument('-i',  '--infile', required=True, help='MGF file')
+    parser.add_argument('-t',  '--table', required=True, help='Table of sequences to compare')
     parser.add_argument('-c', '--config', default=defaultconfig, help='Path to custom config.ini file')
     parser.add_argument('-v', dest='verbose', action='store_true', help="Increase output verbosity")
     args = parser.parse_args()
