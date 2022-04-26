8afe7c8a8575287ee3a1b32a8a2d0bb0e79c8642
Andrea <andrea.laguillo@externo.cnic.es>
2022-04-26 16:52:34 +0200

    cmd test

---
+++ gui/runcmd.js
@@ -0,0 +1,13 @@
+const { spawn } = require(["child_process"]);
+
+spawn("python hello.py", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
