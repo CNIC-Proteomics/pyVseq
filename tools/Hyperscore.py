c49b3f5d2f86f7077c2b89c5b1b64a35be075773
Andrea <andrea.laguillo@externo.cnic.es>
2022-06-23 15:51:31 +0200

    pick matched ions

--- tools/Hyperscore.py
+++ tools/Hyperscore.py
@@ -8,9 +8,16 @@ def _decimal_places(x):
         return 0
     return len(s) - s.index('.') - 1
 
-def hyperscore(ions, matched_ions):
+def hyperscore(ions, proof):
     ## 1. Normalize intensity to 10^5
-    norm = (ions.INT / ions.INT.max()) * 10E4
-    ions["MSF_INT"] = norm
+    ions["MSF_INT"] = (ions.INT / ions.INT.max()) * 10E4
+    
+    ## 2. Pick matched ions ##
+    matched_ions = pd.merge(proof, ions, on="MZ")
+    
+    ## 3. Adjust intenisty
+    matched_ions.MSF_INT = matched_ions.MSF_INT / 10E2
+    
+    ## 4. Hyperscore ##
     
     return(hs)
