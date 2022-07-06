0434d1509919bd6da0f6671cb130a81f836500fa
Andrea <andrea.laguillo@externo.cnic.es>
2022-07-06 09:37:21 +0200

    update hyperscore

--- tools/Hyperscore.py
+++ tools/Hyperscore.py
@@ -13,19 +13,34 @@ def hyperscore(ions, proof):
     ## 1. Normalize intensity to 10^5
     norm = (ions.INT / ions.INT.max()) * 10E4
     ions["MSF_INT"] = norm
-    
     ## 2. Pick matched ions ##
     matched_ions = pd.merge(proof, ions, on="MZ")
-    
     ## 3. Adjust intensity
     matched_ions.MSF_INT = matched_ions.MSF_INT / 10E2
-    
     ## 4. Hyperscore ##
     matched_ions["SERIES"] = matched_ions.apply(lambda x: x.FRAGS[0], axis=1)
-    n_b = matched_ions.SERIES.value_counts()['b']
-    n_y = matched_ions.SERIES.value_counts()['y']
-    i_b = matched_ions[matched_ions.SERIES=='b'].MSF_INT.sum()
-    i_y = matched_ions[matched_ions.SERIES=='y'].MSF_INT.sum()
-    
-    hs = math.log10(math.factorial(n_b) * math.factorial(n_y) * i_b * i_y)
+    matched_ions.FRAGS = matched_ions.FRAGS.str.replace('+', '')
+    matched_ions.FRAGS = matched_ions.FRAGS.str.replace('*', '')
+    temp = matched_ions.copy()
+    # temp = temp.drop_duplicates(subset='FRAGS', keep="first") # Count each kind of fragment only once
+    try:
+        n_b = temp.SERIES.value_counts()['b']
+        i_b = matched_ions[matched_ions.SERIES=='b'].MSF_INT.sum()
+    except KeyError:
+        n_b = 1 # So that hyperscore will not be 0 if one series is missing
+        i_b = 1
+    try:
+        n_y = temp.SERIES.value_counts()['y']
+        i_y = matched_ions[matched_ions.SERIES=='y'].MSF_INT.sum()
+    except KeyError:
+        n_y = 1
+        i_y = 1
+    try:
+        hs = math.log10(math.factorial(n_b) * math.factorial(n_y) * i_b * i_y)
+    except ValueError:
+        hs = 0
     return(hs)
+
+# Problem 1 is that MGF and mzML conversion is different - always use same format for MSFragger and Vseq to compare
+# Problem 2 is that MSFragger does binning and Vseq does not. Build Recom-like with binning to check.
+# MSFragger does *not* take into account b1 and y1 fragments?
