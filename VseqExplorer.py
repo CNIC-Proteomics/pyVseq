360bc855b592a4a83070276385338300acfe8f85
Andrea <andrea.laguillo@externo.cnic.es>
2025-06-05 15:16:24 +0200

    Calculate exp. m/z from exp. MH

--- VseqExplorer.py
+++ VseqExplorer.py
@@ -735,7 +735,7 @@ def main(args):
                     pos = [int(j)-1 for j, k in enumerate(query.Sequence) if k.lower() == '[']
                     ## MZ and MH ##
                     query['expMH'] = query.MH
-                    query['expMZ'] = query.expMH + (m_proton * (query.Charge-1)) / query.Charge
+                    query['expMZ'] = (query.expMH + (m_proton * (query.Charge-1))) / query.Charge
                     query['MZ'] = getTheoMZH(query.Charge, plainseq, mods, pos, True, True, mass)[0]
                     query['MH'] = getTheoMZH(query.Charge, plainseq, mods, pos, True, True, mass)[1]
                     logging.info("\tExploring sequence " + str(query.Sequence) + ", "
